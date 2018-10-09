ccd0fe2797a4affe94811485d644012888935e84
Elizabeth Yunerman <43654442+e-yunerman@users.noreply.github.com>
2018-10-09 13:11:03 -0700

    Added programs made in class.

---
+++ numpy.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+x = 1.0 		#define a float
+y = 2.0 		#define another float
+
+#trigonometry
+print(np.sin(x))			#sin(x)
+print(np.cos(x))			#cos(x)
+print(np.tan(x))			#tan(x)
+print(np.arcsin(x))			#arcsin(x)
+print(np.arccos(x))			#arccos(x)
+print(np.arctan(x))			#arctan(x)
+print(np.arctan2(x,y))		#arctan(x/y)
+print(np.rad2deg(x))		#converts rad to deg
+
+#hyperbolic function
+print(np.sinh(x))
+print(np.cosh(x))
+print(np.tanh(x))
+print(np.arcsinh(x))
+print(np.arccosh(x))
+print(np.arctanh(x))
